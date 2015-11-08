a036e0127e5fe8e305167a757f733e70b08ae1c4
Patrick Walsh <patrick.nilsen.walsh@gmail.com>
2015-11-09 11:34:23 +1300

    bump dependency versions

--- blueprints/ember-cli-ramda-extended/index.js
+++ blueprints/ember-cli-ramda-extended/index.js
@@ -6,11 +6,11 @@ module.exports = {
     return this.addBowerPackagesToProject([
       {
         name: 'ramda-extended',
-        target: '0.3.1'
+        target: '0.3.2'
       },
       {
         name: 'ramda-extended-rsvp',
-        target: '0.1.1'
+        target: '0.1.2'
       }
     ])
   }
