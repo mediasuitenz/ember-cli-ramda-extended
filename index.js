37b2116af0df2e4ee4fff869eaee0cf5303eaf35
Steve Foster <steve.foster@mediasuite.co.nz>
2023-12-20 15:00:17 +1300

    Remove direct bower dependency

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ module.exports = {
   name: 'ember-cli-ramda-extended',
   included (app, parentAddon) {
     this._super.included(app, parentAddon)
-    app.import(app.bowerDirectory + '/ramda-extended/dist/ramda-extended.js')
-    app.import(app.bowerDirectory + '/ramda-extended-rsvp/dist/ramda-extended-rsvp.js')
+    app.import('node_modules/ramda-extended/dist/ramda-extended.js')
+    app.import('node_modules/ramda-extended-rsvp/dist/ramda-extended-rsvp.js')
     app.import('vendor/ramda-ember.js')
   }
 };
